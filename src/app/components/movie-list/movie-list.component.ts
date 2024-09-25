95c64fe4776b8955da752e80164df87ca3fdcd7a
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-25 23:40:26 +0000

    quick fix for app layout

--- src/app/components/movie-list/movie-list.component.ts
+++ src/app/components/movie-list/movie-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { filter, map, Observable } from 'rxjs';
 import { IMedia } from '../../shared/model/media';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
@@ -7,11 +7,12 @@ import { selectFilteredMovie } from '../../shared/state/movie/movie.selectors';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
 import { SearchComponent } from '../../shared/search/search.component';
+import { MovieCardComponent } from "../movie-card/movie-card.component";
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
-  imports: [CommonModule, HeaderComponent, SearchComponent],
+  imports: [CommonModule, HeaderComponent, SearchComponent, MovieCardComponent],
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.scss',
 })
@@ -19,25 +20,34 @@ export class MovieListComponent {
   movies$!: Observable<IMedia[]>;
   category!: string | null;
   isTrending!: boolean;
-  trendingMovies: any = [];
+  trendingMovies: IMedia[] = [];
 
   constructor(private route: ActivatedRoute, private store: Store) {
     console.log(this.category);
   }
 
   ngOnInit(): void {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
     this.route.params.subscribe((params) => {
       this.category = params['category'] || null;
       this.movies$ = this.store.select(selectFilteredMovie(this.category));
-
-      this.movies$.subscribe((movie) => {
-        movie.filter((m) => {
-          this.trendingMovies.push(m.isTrending);
-        });
-      });
-      console.log(this.trendingMovies);
     });
+
+    this.movies$
+      .pipe(map((movies) => movies.filter((movie) => movie.isTrending)))
+      .subscribe((trending) => {
+        this.trendingMovies = trending;
+        this.isTrending = this.trendingMovies.length > 0;
+      });
   }
+
+  // getTredingMovies() {
+  //   this.movies$.pipe(map((movie) => movie)).subscribe((movie) => {
+  //     movie.forEach((movie) => {
+  //       if (movie.isTrending) {
+  //         this.isTrending = true;
+  //         this.trendingMovies.push(movie);
+  //       }
+  //     });
+  //   });
+  // }
 }
