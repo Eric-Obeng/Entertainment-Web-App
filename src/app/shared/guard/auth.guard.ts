3fcf8b6d646b20f7c56c40575ccdf9f7e1f5c69d
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-26 23:56:29 +0000

    creates auth guard

---
+++ src/app/shared/guard/auth.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  GuardResult,
+  MaybeAsync,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { AuthService } from '../services/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.authService.isLoggedIn().pipe(
+      map((loggedIn) => {
+        if (!loggedIn) {
+          this.router.navigate(['/login']);
+        }
+        return loggedIn;
+      })
+    );
+  }
+}
